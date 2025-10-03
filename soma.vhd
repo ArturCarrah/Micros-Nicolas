aca2bebe7cceb06161f9f9e96bd44866331049bd
Artur Carrah <arturcarrah@alu.ufc.br>
2025-10-03 13:31:43 -0300

    Full adder

--- soma.vhd
+++ soma.vhd
@@ -1,33 +1,18 @@
--- Simple OR gate design
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEE.numeric_std.all;
 
-entity xor_gate is
+entity full_adder is
 port(
-  a: in std_logic_vector(2 downto 0);
-  b: in std_logic_vector(2 downto 0);
-  q: out std_logic;
-  c: out std_logic;
-  S: out std_logic_vector(3 downto 0));
- 
-end xor_gate;
+  a, b, cin: IN BIT;
+  s, cout: OUT BIT);
 
-architecture rtl of xor_gate is
+end full_adder;
+
+architecture dataflow of full_adder is
 begin
-  process(a, b) is
-  begin
-  	for i in 0 to 3 loop
-    	S(i) <= 0
-    end loop;
-    
-     c <= 0
-    for i in 0 to 2 loop
-    	S(i) <= (a(i) xor b(i) xor c;
-        wait for 1 ns;
-        c<= a(i) and b(i);
-    end loop;
-    
-   
-  end process;
-end rtl;
+s <= a xor b xor cin;
+  
+cout <= (a and b) OR (a and cin) or (b and cin);
+  
+end dataflow;
