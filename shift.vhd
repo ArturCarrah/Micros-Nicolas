607d47cb6e9e8fe4abddd1dda2e55bf90adcbdbd
YuriAguiar00 <yurifontenele13@gmail.com>
2025-10-27 14:16:22 -0300

    adição do testbanch

--- shift.vhd
+++ shift.vhd
@@ -16,3 +16,46 @@ begin
   outp <= inp(6 downto 0) & '0' when shift = 1 else inp;
   
 end RTL;
+
+-- Testbanch 
+
+entity tb_barrel is
+end tb_barrel;
+
+architecture sim of tb_barrel is
+  
+    signal inp   : std_logic_vector(7 downto 0);
+    signal shift : integer range 0 to 1;
+    signal outp  : std_logic_vector(7 downto 0);
+
+begin
+    uut: entity work.barrel
+        port map (
+            inp   => inp,
+            shift => shift,
+            outp  => outp
+        );
+
+    process
+    begin
+        -- Caso 1: sem deslocar
+        inp <= "10110010";
+        shift <= 0;
+        wait for 10 ns;
+        report "Resultado: " & to_string(outp);
+
+        -- Caso 2: deslocar para esquerda
+        shift <= 1;
+        wait for 10 ns;
+        report "Resultado: " & to_string(outp);
+       
+        -- Caso 3: outro valor de entrada
+        inp <= "00011100";
+        shift <= 1;
+        wait for 10 ns;
+        report "Resultado: " & to_string(outp);
+
+        wait;
+    end process;
+
+end sim;
