eca41ad7fadc5cda0a8f0885beefc1b1a1f10d57
Artur Carrah <arturcarrah@alu.ufc.br>
2025-10-30 21:43:05 -0300

    Shift Left e Shift Right
    Refactor bitshift entity and testbench for clarity and functionality.

--- processador/ULA/shift.vhd
+++ processador/ULA/shift.vhd
@@ -1,61 +1,58 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+entity bitshift is
+  port (
+    inp  : in  std_logic_vector(7 downto 0);
+    shift: in  std_logic;
+    outp : out std_logic_vector(7 downto 0)
+  );
+end bitshift;
 
-entity barrel is
-port (
-  inp: in STD_LOGIC_VECTOR (7 DOWNTO 0);
-  shift: in INTEGER RANGE 0 TO 1;
-  outp: out STD_LOGIC_VECTOR (7 DOWNTO 0));
-end barrel;
-
-
-architecture RTL of barrel is
+architecture RTL of bitshift is
 begin
-
-  outp <= inp(6 downto 0) & '0' when shift = 1 else inp;
-  
+  process (shift, inp)
+  begin
+    if shift = '0' then
+      outp <= inp(6 downto 0) & '0';
+    else
+      outp <= '0' & inp(7 downto 1);  
+    end if;
+  end process;
 end RTL;
 
--- Testbanch 
+-- Testbench 
 
-entity tb_barrel is
-end tb_barrel;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-architecture sim of tb_barrel is
-  
-    signal inp   : std_logic_vector(7 downto 0);
-    signal shift : integer range 0 to 1;
+entity tb_shift is
+end tb_shift;
+
+architecture sim of tb_shift is
+    signal inp : std_logic_vector(7 downto 0);
+    signal shift : std_logic;
     signal outp  : std_logic_vector(7 downto 0);
-
 begin
-    uut: entity work.barrel
+    uut: entity work.bitshift
         port map (
-            inp   => inp,
-            shift => shift,
-            outp  => outp
+            inp    => inp,
+            shift  => shift,
+            outp => outp
         );
 
     process
     begin
-        -- Caso 1: sem deslocar
-        inp <= "10110010";
-        shift <= 0;
+        -- Teste 1: Shift Left
+        inp <= "01010001"; shift <= '0';
         wait for 10 ns;
-        report "Resultado: " & to_string(outp);
+        assert(outp = "10100010") report "Fail shl saida" severity error;
 
-        -- Caso 2: deslocar para esquerda
-        shift <= 1;
+        -- Teste 2: Shift Right
+        inp <= "01010001"; shift <= '1';
         wait for 10 ns;
-        report "Resultado: " & to_string(outp);
-       
-        -- Caso 3: outro valor de entrada
-        inp <= "00011100";
-        shift <= 1;
-        wait for 10 ns;
-        report "Resultado: " & to_string(outp);
-
-        wait;
+        assert(outp = "00101000") report "Fail shr saida" severity error;
+        
+        wait; 
     end process;
-
 end sim;
