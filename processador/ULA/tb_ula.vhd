4838871f97b17a434826c6644c1d8db431b47002
Artur Carrah <arturcarrah@alu.ufc.br>
2025-11-01 14:54:59 -0300

    Testbench da ULA

--- processador/ULA/tb_ula.vhd
+++ processador/ULA/tb_ula.vhd
@@ -23,7 +23,8 @@ architecture sim of testbench is
     signal op1_tb       : std_logic_vector(7 downto 0);
     signal op2_tb       : std_logic_vector(7 downto 0);
     signal r_tb         : std_logic_vector(7 downto 0);
-
+	signal run_clk: std_logic := '1';
+    
     -- Clock period
     constant CLK_PERIOD : time := 10 ns;
 
@@ -38,40 +39,165 @@ begin
             r         => r_tb
         );
 
-        clock_gen: process
-        begin
-            for i in 0 to 40 loop  -- 40 ciclos de clock
-                clk_tb <= '0';
-                wait for 5 ns;
-                clk_tb <= '1';
-                wait for 5 ns;
-            end loop;
-            wait; -- encerra o processo
-        end process;
+    -- Geração de clock
+    clock_gen: process
+    begin
+    	
+        loop
+            clk_tb <= '0';
+            wait for CLK_PERIOD / 2;
+            clk_tb <= '1';
+            wait for CLK_PERIOD / 2;
+            
+            if run_clk = '0' then
+            	exit; 
+        	end if;
+        end loop;
+        wait;
+    end process;
 
-    -- Estímulos
     stim_proc : process
     begin
-        -- Teste 1: operação ADD ("00000100")
+        -----------------------------------------------------------------
+        -- MOV
+        -----------------------------------------------------------------
+        operation_tb <= "00000000";
+        op1_tb <= "00001111"; -- 15
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida MOV: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- SHIFT ESQUERDA
+        -----------------------------------------------------------------
+        operation_tb <= "00000010";
+        wait until rising_edge(clk_tb);  
+        op1_tb <= "00001101"; -- 13
+        wait until rising_edge(clk_tb);
+        wait until rising_edge(clk_tb);  
+        wait for 1 ns;
+        report "Saida SHL: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- SHIFT DIREITA
+        -----------------------------------------------------------------
+        operation_tb <= "00000011";
+        wait until rising_edge(clk_tb);  
+        op1_tb <= "00001010"; -- 10
+        wait until rising_edge(clk_tb);
+        wait until rising_edge(clk_tb); 
+        wait for 1 ns;
+        report "Saida SHR: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- ADD
+        -----------------------------------------------------------------
         operation_tb <= "00000100";
         op1_tb <= "00001010"; -- 10
         op2_tb <= "00000101"; -- 5
         wait until rising_edge(clk_tb);
-        
-        -- Teste 2: operação MUL ("00000110")
+        wait for 1 ns;
+        report "Saida ADD: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- MUL
+        -----------------------------------------------------------------
         operation_tb <= "00000110";
         op1_tb <= "00000011"; -- 3
         op2_tb <= "00000100"; -- 4
-       	wait until rising_edge(clk_tb);
-
-        -- Teste 3: operação ADD novamente
-        operation_tb <= "00000100";
-        op1_tb <= "00001111"; -- 15
-        op2_tb <= "00000001"; -- 1
         wait until rising_edge(clk_tb);
-        
-        -- Finaliza simulação
-        wait for 20 ns;
+        wait for 1 ns;
+        report "Saida MUL: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- DIV
+        -----------------------------------------------------------------
+        operation_tb <= "00000111";
+        op1_tb <= "00001010"; -- 10
+        op2_tb <= "00000010"; -- 2
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida DIV: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- MOD
+        -----------------------------------------------------------------
+        operation_tb <= "00001000";
+        op1_tb <= "00001010"; -- 10
+        op2_tb <= "00000110"; -- 6
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida MOD: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- COMPARADOR
+        -----------------------------------------------------------------
+        -- A < B
+        operation_tb <= "00001001";
+        op1_tb <= "00000101"; -- 5
+        op2_tb <= "00001000"; -- 8
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida CMP_S: " & to_string(r_tb);
+
+        -- A > B
+        operation_tb <= "00001001";
+        op1_tb <= "00001111"; -- 15
+        op2_tb <= "00001000"; -- 8
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida CMP_G: " & to_string(r_tb);
+
+        -- A = B
+        operation_tb <= "00001001";
+        op1_tb <= "00000110"; -- 6
+        op2_tb <= "00000110"; -- 6
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida CMP_E: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- AND
+        -----------------------------------------------------------------
+        operation_tb <= "00001010";
+        op1_tb <= "00001100"; -- 12 (1100)
+        op2_tb <= "00001010"; -- 10 (1010)
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida AND: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- OR
+        -----------------------------------------------------------------
+        operation_tb <= "00001011";
+        op1_tb <= "00001100"; -- 12
+        op2_tb <= "00001010"; -- 10
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida OR: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- NOT
+        -----------------------------------------------------------------
+        operation_tb <= "00001100";
+        op1_tb <= "00000000"; -- 0
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida NOT: " & to_string(r_tb);
+
+        -----------------------------------------------------------------
+        -- XOR
+        -----------------------------------------------------------------
+        operation_tb <= "00001101";
+        op1_tb <= "00001100"; -- 12 (1100)
+        op2_tb <= "00001010"; -- 10 (1010)
+        wait until rising_edge(clk_tb);
+        wait for 1 ns;
+        report "Saida XOR: " & to_string(r_tb);
+
+        wait for 10 ns;
+        run_clk <= '0';
         wait;
     end process;
 end sim;
+
