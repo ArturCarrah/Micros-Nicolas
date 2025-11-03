5e26c8c425a21d19f16ff725b066e3f6aa5b2f80
Artur Carrah <arturcarrah@alu.ufc.br>
2025-11-02 21:03:52 -0300

    Refactor processador.vhd to improve architecture

--- processador/processador.vhd
+++ processador/processador.vhd
@@ -1,70 +1,159 @@
 library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-entity processador is
-    port(
-        clk_p   : in std_logic;
-        rst_p   : in std_logic
+entity design is
+    Port (
+        clk : in  std_logic;
+        rst : in  std_logic;
+
+        r0_dbg : out std_logic_vector(7 downto 0);
+        r1_dbg : out std_logic_vector(7 downto 0);
+        r2_dbg : out std_logic_vector(7 downto 0);
+        r3_dbg : out std_logic_vector(7 downto 0);
+        r4_dbg : out std_logic_vector(7 downto 0);
+        r5_dbg : out std_logic_vector(7 downto 0);
+        r6_dbg : out std_logic_vector(7 downto 0);
+        r7_dbg : out std_logic_vector(7 downto 0)
     );
-end entity;
+end entity design;
 
-architecture main of processador is
-    signal md_data_out  : std_logic_vector(7 downto 0);
-    signal mp_data_out  : std_logic_vector(7 downto 0);
-    signal mp_addr      : std_logic_vector(7 downto 0);
-    signal md_addr      : std_logic_vector(7 downto 0);
+architecture Behavioral of design is
 
-    signal reg_write    : std_logic;
-    signal addr_dest    : std_logic_vector(2 downto 0);
-    signal addr_op1     : std_logic_vector(2 downto 0);
-    signal addr_op2     : std_logic_vector(2 downto 0);
-    signal data_in_reg  : std_logic_vector(7 downto 0);
-    signal op1_val      : std_logic_vector(7 downto 0);
-    signal op2_val      : std_logic_vector(7 downto 0);
+    component control_unit
+        port (
+            clk_uc        : in  std_logic;
+            rst_uc        : in  std_logic;
+            mp_addr       : out std_logic_vector(7 downto 0);
+            mp_data_out   : in  std_logic_vector(7 downto 0);
+            md_addr       : out std_logic_vector(7 downto 0);
+            md_data_out   : in  std_logic_vector(7 downto 0);
+            addr_dest     : out std_logic_vector(2 downto 0);
+            addr_op1      : out std_logic_vector(2 downto 0);
+            addr_op2      : out std_logic_vector(2 downto 0);
+            valor_op1     : in  std_logic_vector(7 downto 0);
+            valor_op2     : in  std_logic_vector(7 downto 0);
+            reg_write     : out std_logic;
+            dado_escrita  : out std_logic_vector(7 downto 0)
+        );
+    end component;
 
+    component register_file
+        port (
+            clk        : in  std_logic;
+            rst        : in  std_logic;
+
+            we         : in  std_logic;
+            wr_addr    : in  std_logic_vector(2 downto 0);
+            data_in    : in  std_logic_vector(7 downto 0);
+
+            rd_addr_1  : in  std_logic_vector(2 downto 0);
+            data_out_1 : out std_logic_vector(7 downto 0);
+
+            rd_addr_2  : in  std_logic_vector(2 downto 0);
+            data_out_2 : out std_logic_vector(7 downto 0);
+
+            r0, r1, r2, r3, r4, r5, r6, r7 : out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+
+    component memoria_programa
+        port (
+            clk      : in  std_logic;
+            rst      : in  std_logic;
+            addr     : in  std_logic_vector(7 downto 0);
+            data_out : out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+    component memoria_dados
+        port (
+            clk      : in  std_logic;
+            rst      : in  std_logic;
+            addr     : in  std_logic_vector(7 downto 0);
+            data_out : out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+    
+    signal mp_addr_sig     : std_logic_vector(7 downto 0);
+    signal mp_data_out_sig : std_logic_vector(7 downto 0);
+    signal md_addr_sig     : std_logic_vector(7 downto 0);
+    signal md_data_out_sig : std_logic_vector(7 downto 0);
+    
+    signal addr_dest_sig   : std_logic_vector(2 downto 0); --Endereço de escrita
+    signal addr_op1_sig    : std_logic_vector(2 downto 0); --Endereço de leitura 1
+    signal addr_op2_sig    : std_logic_vector(2 downto 0); --Endereço de Leitura 2
+    signal reg_write_sig   : std_logic;                    --Write enable
+    signal dado_escrita_sig: std_logic_vector(7 downto 0);
+    
+
+    signal valor_op1_sig   : std_logic_vector(7 downto 0);
+    signal valor_op2_sig   : std_logic_vector(7 downto 0); 
+    
 begin
-    imemp: entity work.memoria_programa
-        port map (
-            addr     => mp_addr,
-            data_out => mp_data_out
+
+    CU: control_unit
+        port map(
+            clk_uc      => clk,
+            rst_uc      => rst,
+            mp_addr     => mp_addr_sig,
+            mp_data_out => mp_data_out_sig,
+            md_addr     => md_addr_sig,
+            md_data_out => md_data_out_sig,
+            
+            -- Saídas de controle para o RF
+            addr_dest   => addr_dest_sig,
+            addr_op1    => addr_op1_sig,
+            addr_op2    => addr_op2_sig,
+            reg_write   => reg_write_sig,
+            dado_escrita => dado_escrita_sig,
+            
+            -- Entradas de dados para a ULA (dentro da UC)
+            valor_op1   => valor_op1_sig,
+            valor_op2   => valor_op2_sig
         );
 
-    imemd: entity work.memoria_dados
-        port map (
-            addr     => md_addr,
-            data_out => md_data_out
+    PM: memoria_programa
+        port map(
+            clk      => clk,
+            rst      => rst,
+            addr     => mp_addr_sig,
+            data_out => mp_data_out_sig
         );
 
-    iregf: entity work.register_file
-        port map (
-            clk        => clk_p,
-            rst        => rst_p,
-            we         => reg_write,
-            addr_dest  => addr_dest,
-            addr_op1   => addr_op1,
-            addr_op2   => addr_op2,
-            data_in    => data_in_reg,
-            data_out1  => op1_val,
-            data_out2  => op2_val,
-            r0 => open, r1 => open, r2 => open, r3 => open,
-            r4 => open, r5 => open, r6 => open, r7 => open
+    DM: memoria_dados
+        port map(
+            clk      => clk,
+            rst      => rst,
+            addr     => md_addr_sig,
+            data_out => md_data_out_sig
         );
 
-    iuc: entity work.control_unit
-        port map (
-            clk_uc        => clk_p,
-            rst_uc        => rst_p,
-            mp_addr       => mp_addr,
-            mp_data_out   => mp_data_out,
-            md_addr       => md_addr,
-            md_data_out   => md_data_out,
-            addr_dest     => addr_dest,
-            addr_op1      => addr_op1,
-            addr_op2      => addr_op2,
-            valor_op1     => op1_val,
-            valor_op2     => op2_val,
-            reg_write     => reg_write,
-            dado_escrita  => data_in_reg
+    RF: register_file
+        port map(
+            clk        => clk,
+            rst        => rst,
+            
+            we         => reg_write_sig,
+            wr_addr    => addr_dest_sig,
+            data_in    => dado_escrita_sig,
+            
+            rd_addr_1  => addr_op1_sig,
+            data_out_1 => valor_op1_sig, 
+            
+            rd_addr_2  => addr_op2_sig,
+            data_out_2 => valor_op2_sig,  
+            
+            r0         => r0_dbg,
+            r1         => r1_dbg,
+            r2         => r2_dbg,
+            r3         => r3_dbg,
+            r4         => r4_dbg,
+            r5         => r5_dbg,
+            r6         => r6_dbg,
+            r7         => r7_dbg
         );
-end architecture;
+
+end Behavioral;
